ab0329339de5bc736912e68dd7ea9f90cf7dbc80
Muhammad-Umar Yoqubjonov <Muhammad-Umar.Yoqubjonov>
2024-02-06 16:48:30 +0500

    setting back

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
 var express = require('express')
 const routes = require('./routes')
-const mongoose = require("mongoose")
+const mongoose = require('mongoose')
 
 var app = express()
-app.use("/", routes)
+
+var cors = require('cors')
+
+app.use(cors())
+
+app.use('/', routes)
+
+app.use('/images', express.static('images')) // to open image on web through url
 
 const getProducts = async () => {
   try {
-    await mongoose.connect('mongodb+srv://yoqubjonov:GR3oyT0ZUu2tPto8@volto.xo2a7kc.mongodb.net/voltodb?retryWrites=true&w=majority')
-    app.listen(5000, () => console.log("listen on port 5000"))
-  } catch(err) {
-    console.log(err);
+    await mongoose.connect(
+      'mongodb+srv://yoqubjonov:GR3oyT0ZUu2tPto8@volto.xo2a7kc.mongodb.net/voltodb?retryWrites=true&w=majority'
+    )
+    app.listen(5000, () => console.log('listen on port 5000'))
+  } catch (err) {
+    console.log(err)
   }
 }
 
--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,6 @@ const { Router } = require('express')
 
 const router = new Router()
 
-// var express = require('express')
-// var cors = require('cors')
-
-// var app = express()
-// app.use(cors())
-
 const ProductController = require('../controllers/ProductController')
 
 router.get('/api/products/getProducts', ProductController.getProducts)
